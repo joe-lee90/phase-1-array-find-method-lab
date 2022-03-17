07951a9108d7fb4dd753fdced68c017745912304
Joe <zvmboni@gmail.com>
2022-03-17 08:47:23 -0700

    commit tree fff3c3e6e31f6cb9840cab95f3be82ebb7d7fc90

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(array){
+    const result = array.find(cbHelper);
+
+    if(result){
+        return result.year;
+    }
+
+    return;
+}
+
+function cbHelper(array){
+    return (array.result === 'W');
+}
